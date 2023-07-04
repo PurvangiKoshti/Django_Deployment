5e179ab0b178e420feda8bf8c4668f8bd2d19376
Purvangi Koshti <purvangi.koshti@cctech.co.in>
2023-07-04 13:10:56 +0530

    chnaged urls for both application

--- django_project/urls.py
+++ django_project/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('app/',include('travello.urls')),
+    path('app/',include('myapp.urls')),
+    path('travello/',include('travello.urls')),
     path("admin/", admin.site.urls),
 ]
